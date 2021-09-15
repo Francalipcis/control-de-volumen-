1e0b762af0bc59b150dc22a956343e5caddcc842
Francalipcis <78613986+Francalipcis@users.noreply.github.com>
2021-09-15 18:19:25 -0300

    Add files via upload

---
+++ control de volumen_.py
@@ -0,0 +1,79 @@
+import cv2 
+import mediapipe
+import time
+import numpy as np
+import hand_traquin_module as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+wCam, hCam = 1080, 720
+
+
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+pTime = 0
+
+
+detector = htm.handDetector(detectionCon=0.7)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+volumeRange = volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(-20.0, None)
+minVol = volumeRange[0]
+maxVol = volumeRange[1]
+
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    
+    if len(lmList) != 0:
+        #print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+
+        cx,cy = (x1+x2)//2, (y1+y2)//2 
+
+
+        cv2.circle(img, (x1,y1), 7, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x2,y2), 7, (255,0,255), cv2.FILLED)
+        cv2.line(img, (x1,y1), (x2,y2), (255,0,255), 3)
+        cv2.circle(img, (cx,cy), 7, (255,0,255), cv2.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+        #print(length)
+
+
+        # hand range 50-300
+        #volume range -64 - 0
+
+        vol = np.interp(length,[50,300], [minVol,maxVol])
+        print(int(length), vol)
+        volume.SetMasterVolumeLevel(vol, None)
+
+
+
+        if length < 50:
+            cv2.circle(img, (cx,cy), 7, (0,255,0), cv2.FILLED)
+
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    cv2.imshow("img", img)
+    cv2.waitKey(1)
